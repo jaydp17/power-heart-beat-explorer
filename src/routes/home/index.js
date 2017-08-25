a43f9a37ee3ef3f66a554f6d3610e64157b73259
Jaydeep Solanki <jaydp17@gmail.com>
2017-08-25 17:40:04 +0530

    draw a sample chart

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,15 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import Chart from '../../components/chart';
+
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
+				<Chart />
 			</div>
 		);
 	}
