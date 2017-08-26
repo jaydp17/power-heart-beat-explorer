3eceaaa3f1323ab5d0fc461fe37be51e001a9d8d
Jaydeep Solanki <jaydp17@gmail.com>
2017-08-27 03:18:54 +0530

    don't check for current minute

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,8 @@ import moment from 'moment';
 
 // eslint-disable-next-line import/prefer-default-export
 export function getKeysForPastMinutes(minutes) {
-  const now = moment.utc();
+  // don't check from the current time, as the PI might have not sent the data yet
+  const now = moment.utc().subtract(1, 'minute');
   const results = [];
   for (let i = 0; i < minutes; i++) {
     // here haven't switched to dev-powerData prefix as I plan to use the data from prod
