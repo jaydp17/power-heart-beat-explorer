81d092948a67f69be023951ae1a4517bda3a71ff
Jaydeep Solanki <jaydp17@gmail.com>
2017-08-27 02:41:29 +0530

    display with emoji if power is there or not

--- src/components/IsPowerThere/IsPowerThere.js
+++ src/components/IsPowerThere/IsPowerThere.js
@@ -1,8 +1,40 @@
 import { h } from 'preact';
 
-export default ({ isLoading, hasPower }) =>
-  <div>
-    {isLoading ? 'Loading...' : ''}
-    <br />
-    {hasPower ? 'Yes' : 'No'}
-  </div>;
+import style from './style.scss';
+
+export default ({ isLoading, hasPower }) => {
+  const emojiCodes = {
+    bulb: '1f4a1',
+    candle: '1f56f',
+    hourglass: '23f3',
+  };
+
+  const currentEmoji = hasPower ? emojiCodes.bulb : emojiCodes.candle;
+  const currentText = hasPower ? 'Yes' : 'No';
+
+  const loadingView = isLoading
+    ? <div>
+        <img
+          class={style.loadingImg}
+          src={`https://twemoji.maxcdn.com/2/svg/${emojiCodes.hourglass}.svg`}
+          height="150"
+        />
+        <div class={style.loadingText}>Loading…</div>
+      </div>
+    : null;
+
+  const actualView = isLoading
+    ? null
+    : <div>
+        <img src={`https://twemoji.maxcdn.com/2/svg/${currentEmoji}.svg`} height="350" />
+        <div class={style.statusText}>
+          {currentText}
+        </div>
+      </div>;
+
+  return (
+    <div class={style.container}>
+      {isLoading ? loadingView : actualView}
+    </div>
+  );
+};
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,7 +5,5 @@ import IsPowerThere from '../../components/IsPowerThere';
 
 export default () =>
   <div class={style.home}>
-    <h1>Home</h1>
-    <p>This is the Home component.</p>
     <IsPowerThere />
   </div>;
